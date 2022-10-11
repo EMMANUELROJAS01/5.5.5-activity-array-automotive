7796d4c32960f0af16f0e8233aafc94deb197f1f
EMMANUELROJAS01 <392rojas@gmail.com>
2022-10-10 23:28:50 -0700

    changed font size on navbar

--- vehicle.js
+++ vehicle.js
@@ -9,6 +9,7 @@ class Vehicle {
         this.mileage = mileage;
         this.started = false;
         this.numberOfWheels = 0;
+        this.fuel = 100
     }
 
     start() {
@@ -52,6 +53,7 @@ class Vehicle {
         }
     }
     stop() {
+        console.log('engine off')
         this.started = false;
     }
 
@@ -73,11 +75,11 @@ class Vehicle {
     }
 
     typeOfVehicle(wheels) {
-        if (this.numberOfWheels == 8 && 8 == wheels) {
+        if (this.numberOfWheels == 8) {
             console.log(this.model + " " + this.make + " is a Truck");
-        } else if (this.numberOfWheels == 4 && 4 == wheels) {
+        } else if (this.numberOfWheels == 4) {
             console.log(this.model + " " + this.make + " is a CAr");
-        } else if (this.numberOfWheels == 2 && 2 == wheels) {
+        } else if (this.numberOfWheels == 2) {
             console.log(this.model + " " + this.make + " is a Bike");
         } else {
             console.log("Unknown type of vehicle");
